6b3c22c07eb4fc06324581cf9fb0fdb0af8b579e
Albine k Jibu <albinekjibu57@gmail.com>
2025-02-19 16:01:55 +0530

    check file

--- expensetrackerapp/models.py
+++ expensetrackerapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        abstract = True
+
+class Category(TimeStampedModel):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+class Budget(TimeStampedModel):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    limit_amount = models.DecimalField(max_digits=10,decimal_places=2)
+
+# #class Alert(TimeStampedModel):
+#     user_id  = models.Foreignkey(User, on_delete=models.CASCADE)
+#     alert_type = models.CharField(max_length=20)
+#     related_budget_id = models.ForeignKey(Budget, on_delete=models.CASECADE)
+#     message = models.CharField()
+#     is_read = models.BooleanField()
+    
+
