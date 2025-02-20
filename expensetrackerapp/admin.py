fa1918610de69e09b143b932057908b2220f7405
Albine k Jibu <albinekjibu57@gmail.com>
2025-02-20 14:59:08 +0530

    check file

--- expensetrackerapp/admin.py
+++ expensetrackerapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category,Budget
+from .models import Category,Budget,Expense
 admin.site.register(Category)
 admin.site.register(Budget)
+admin.site.register(Expense)
 
 
 # Register your models here.
--- expensetrackerapp/models.py
+++ expensetrackerapp/models.py
@@ -17,12 +17,20 @@ class Category(TimeStampedModel):
         return self.name
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
-    description = models.TextField()
+    description = models.TextField(max_length=20)
 
 class Budget(TimeStampedModel):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     limit_amount = models.DecimalField(max_digits=10,decimal_places=2)
+
+class Expense(TimeStampedModel):
+        user_id= models.ForeignKey(User, on_delete=models.CASCADE)
+        Category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+        amount = models.DecimalField(max_digits=10,decimal_places=2)
+        expense_date = models.DateTimeField(auto_now_add=True)
+        description = models.TextField(max_length=50)
+
 
 # #class Alert(TimeStampedModel):
 #     user_id  = models.Foreignkey(User, on_delete=models.CASCADE)
