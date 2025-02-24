c25615c20683daa3027f4d246d70125e413c0931
Alan Benny <alanbennynedumkattil05@gmail.com>
2025-02-24 23:08:17 +0530

    check file

--- expensetrackerproject/settings.py
+++ expensetrackerproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'expensetrackerproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'expense_tracker_db',
+        'USER': 'root',
+        'PASSWORD': 'KL05Z2656',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
